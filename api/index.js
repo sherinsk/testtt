f822890bbaba2d7ea3bfc2876eff301bb3a5cf0e
Correct Username <correct@example.com>
2024-05-22 13:34:37 +0530

    fourth commit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const prisma = new PrismaClient();
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
